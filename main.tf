5f81b3426d2b4377427d333c0eac8aea059ee677
harodggg <haroldtiansheng@gmail.com>
2023-10-11 22:40:17 +0800

    init project,add vpc_network_a

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+
+  }
+
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "a_network"
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+  default = "gcp-fleek"
+}
+
+variable "credentials_file" {
+  default = "./gcp-fleek-server.json"
+}
+
+variable "region" {
+  default = "us-central1"
+
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
